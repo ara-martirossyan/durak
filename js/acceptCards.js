92ef237f543c9f05cd18652705414b38e8c2ad32
Ara <martirossyan.ara@gmail.com>
2015-11-12 17:50:45 +0100

    accept cards in pc hand

--- js/acceptCards.js
+++ js/acceptCards.js
@@ -1,3 +1,6 @@
+/**
+* accept cards on table into "my-hand"
+*/
 var acceptCardsToMyHand = function(time){	
  	var table = [].map.call(grabCards("table"), function(e){return e})
   	var prepared = prepare(table.length, "table", "my-hand", trump(), time/2)
@@ -19,6 +22,46 @@ var acceptCardsToMyHand = function(time){
 	    }) 
 	},time/2) 
 
+}
+
+/**
+* shift cards in pc hand to the right side
+* in order to accept cards from table 
+* and returns the array that would be formed after accepting
+*/
+var pcHandMakeSpaceToAccept = function(time){
+	var target = grabCards("pc-hand").add( grabCards("table") );
+	target  = [].map.call( target,  function(e){return e}  )
+	var scale = handScale(target);
+	grabCards("pc-hand").each(function(idx,card){	
+		$(card).animate(
+			{left: target.indexOf(card)*scale+"em", top: 0+"em"},
+			{
+				duration: time,
+				queue: false,
+			}
+		);
+	});
+	return target;
+}
+
+/**
+* accept cards on table into "my-hand"
+*/
+var acceptCardsToPcHand = function(time){
+	var table = [].map.call(grabCards("table"), function(e){return e})
+	var target = pcHandMakeSpaceToAccept(time);
+	var scale = handScale(target);
+	setTimeout(function(){
+		table.forEach(function(cardNode){
+			var offY = Number( cardNode.style.top.replace(/[^0-9,.,-]/g, '') );
+			var card = insert(cardNode, target, "table", "pc-hand")
+			.css({top: tableTopFromPcHand() + offY + 'em', left: cardNode.style.left })
+			.dequeue()
+			var idxCard = target.indexOf(cardNode);
+	   		moveByFlip(card, idxCard*scale, 0, time, false)
+		})
+	},time)
 }
 
 
