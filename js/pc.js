f41f1356faed3577dead111b2c9ab9f319be6b37
ara-martirssyan <martirossyan.ara@gmail.com>
2015-12-06 02:12:29 +0100

    adding functionality of cards when accepting/collecting into pc-hand

--- js/pc.js
+++ js/pc.js
@@ -1,9 +1,6 @@
 var pcTurn = function(time){
 	var status = statusOfGame();
-	var attackingCards;	
-	var defendingCards;
-
-	var cardNode;
+	var attackingCards, defendingCards, cardNode;	
 	if(status === "attack" && getValidCardsToAttack("pc-hand").length !== 0){
 		attackingCards = getValidCardsToAttack("pc-hand") ;
 		cardNode = attackingCards[ attackingCards.length - 1 ];
@@ -16,6 +13,42 @@ var pcTurn = function(time){
 		discard(time);
 	}else if(status === "defend"){//no valid cards to defend
 		promptMessage( "I accept the cards. You can add additional cards" )
-		acceptCardsToPcHand(time);
+		addCardsForPcToCollect(time);
 	}
 }
+
+
+var addCardsForPcToCollect = function(time){
+	createButton( document.body , "finish adding", "btn1", function(){
+			promptMessage("");
+			acceptCardsToPcHand(time);
+			disableTurn();
+	})
+
+	getArray("my-hand").forEach(function(elem){
+		$(elem).mouseover(function(){
+			$(this).css({'top': popUp()+'em'});
+		});
+		$(elem).mouseout(function(){
+			$(this).css({'top': '0em'});
+		});
+		$(elem).click(function(){   
+		    var cardNode = $(this).get(0);
+            validateCardAndAddForCollection(cardNode,time);
+	    });
+	})
+}
+
+var validateCardAndAddForCollection = function(cardNode, time){
+	if( isValidCardToAttack(cardNode) ){
+		promptMessage("");
+		disableTurn();
+		addToCollect(cardNode,time);
+		setTimeout(function(){
+			addCardsForPcToCollect(time);
+		},time)
+	}else{
+		promptMessage( "There are no " + rankWithWordsInPlural(cardNode) + " on the table, choose a valid card or finish adding");
+	}
+}
+
